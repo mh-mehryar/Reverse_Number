46a7b11c9b274485fd1278f2c728bef9400c8509
mh-mehryar <44663511+mh-mehryar@users.noreply.github.com>
2021-11-05 19:06:36 +0330

    Revers_Number

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Revers_Number
+{
+    class Program
+    {
+                static void Main(string[] args)
+        {
+            
+            int res = reversNum();
+            Console.WriteLine("The Revers of Number you Entered is : "  + res);
+        }
+        public static int reversNum()
+        {
+            int num, reverse = 0, remain;
+            Console.Write("Enter a number: ");
+            num = int.Parse(Console.ReadLine());
+            while (num != 0)
+            {
+                remain = num % 10;
+                reverse = reverse * 10 + remain;
+                num = num / 10;
+            }
+           
+            return reverse;
+        }
+    }
+}
